b8ac715a4ab1675134e3c925a81aa5a8f5a26fbc
SalvadorCT <salvadorlinc@gmail.com>
2022-10-28 23:41:59 -0500

    .env

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const routerApi = require("./routes");
+const dotenv = require("dotenv");
 //declaro faker , previamen6e instalado
 /* const { faker } = require('@faker-js/faker'); */
-
+dotenv.config();
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 5000;
 
 //para pos es necesario usar midelware
 app.use(express.json());
